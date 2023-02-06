22cf2f317084909b8e2b86a10a576d75278bd5e2
TiparpronS <Sukanyat2022a@gmail.com>
2023-02-06 10:10:03 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, Grid, GridItem, Input, Text } from '@chakra-ui/react';
 import { FaSearch } from 'react-icons/fa';
 //  above search icon from react -import
@@ -6,6 +6,9 @@ import { FaSearch } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
+  // eslint-disable-next-line
+  const [value,setValue] = useState(1);
+
   return (
     //this box control the whole box
     <Box
@@ -84,101 +87,203 @@ const App = () => {
 
       {/* sectionbelow component below-pictures*/}
       <Box  marginTop="20px">
-        <h2 fontWeight="bold">Mountain Pictures</h2>
+        <h2 fontWeight="bold" fontSize="20px">Mountain Pictures</h2>
       </Box>
+      {/* this is first box for mountain */}
+      {
+        // eslint-disable-next-line
+     (value = 1)?
       <Box
         display="grid"
         justifyItems="center"
-     
-        height="550px"
+        height="500px"
+        width="80%"
+        marginTop="20px"
+      >
+        <Box>Hello</Box>
+        <Grid templateColumns="repeat(4, 1fr)" gap={3} placeItems="center">
+          <GridItem w="100%" h="150">
+         
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
+              h="150"
+              w="45px"
+            />
+          </GridItem>
+          <GridItem w="100%" h="150">
+         
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
+              h="45px"
+              w="45px"
+            />
+          </GridItem>
+          <GridItem w="100%" h="150">
+         
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
+              h="45px"
+              w="45px"
+            />
+          </GridItem>
+          <GridItem w="100%" h="150">
+         
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://images.unsplash.com/photo-1486870591958-9b9d0d1dda99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
+              h="45px"
+              w="45px"
+            />
+          </GridItem>
+
+          <GridItem w="100%" h="150">
+         
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
+              h="45px"
+              w="45px"
+            />
+          </GridItem>
+          <GridItem w="100%" h="150">
+         
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+              alt="mountain"
+              h="45px"
+              w="45px"
+            />
+          </GridItem>
+          <GridItem w="100%" h="50%">
+         
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+              alt="mountain"
+              h="45px"
+              w="45px"
+            />
+          </GridItem>
+          <GridItem w="100%" h="150">
+         
+            <img
+              style={{ height: '200px', width: '200px' }}
+              src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+              alt="mountain"
+              h="45px"
+              w="45px"
+            />
+          </GridItem>
+        </Grid>
+      </Box>
+
+    :
+      <Box
+        display="grid"
+        justifyItems="center"
+        height="500px"
         width="80%"
         marginTop="20px"
       >
         <Grid templateColumns="repeat(4, 1fr)" gap={3} placeItems="center">
           <GridItem w="100%" h="150">
-            {' '}
+         
             <img
               style={{ height: '200px', width: '200px' }}
-              src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              alt="image"
+              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
               h="150"
               w="45px"
-            />{' '}
+            />
           </GridItem>
           <GridItem w="100%" h="150">
-            {' '}
+         
             <img
               style={{ height: '200px', width: '200px' }}
-              src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              alt="image"
+              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
+           
               h="45px"
               w="45px"
-            />{' '}
+            />
           </GridItem>
           <GridItem w="100%" h="150">
-            {' '}
+         
             <img
               style={{ height: '200px', width: '200px' }}
-              src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              alt="image"
+              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
+           
               h="45px"
               w="45px"
-            />{' '}
+            />
           </GridItem>
           <GridItem w="100%" h="150">
-            {' '}
+         
             <img
               style={{ height: '200px', width: '200px' }}
-              src="https://images.unsplash.com/photo-1486870591958-9b9d0d1dda99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              alt="image"
+              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
+           
               h="45px"
               w="45px"
-            />{' '}
+            />
           </GridItem>
 
           <GridItem w="100%" h="150">
-            {' '}
+         
             <img
               style={{ height: '200px', width: '200px' }}
-              src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-              alt="image"
+              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
               h="45px"
               w="45px"
-            />{' '}
+            />
           </GridItem>
           <GridItem w="100%" h="150">
-            {' '}
+         
             <img
               style={{ height: '200px', width: '200px' }}
-              src="https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-              alt="image"
+              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
               h="45px"
               w="45px"
-            />{' '}
+            />
           </GridItem>
           <GridItem w="100%" h="50%">
-            {' '}
+         
             <img
               style={{ height: '200px', width: '200px' }}
-              src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-              alt="image"
+              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
               h="45px"
               w="45px"
-            />{' '}
+            />
           </GridItem>
           <GridItem w="100%" h="150">
-            {' '}
+         
             <img
               style={{ height: '200px', width: '200px' }}
-              src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG1vdW50YWluc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-              alt="image"
+              src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+              alt="mountain"
               h="45px"
               w="45px"
-            />{' '}
+            />
           </GridItem>
         </Grid>
       </Box>
+      }
     </Box>
+
   );
 };
 
---
+++ src/test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <div>
+      {
+        value = 1 ? "hello" : "welcome"
+      }
+    </div>
+  )
+}
+
+export default test
